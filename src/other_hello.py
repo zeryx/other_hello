bcafa0a7a371ec721efb0b4a7a4b33273d9e6263
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-01-09 19:00:04 -0400

    real change

--- src/other_hello.py
+++ src/other_hello.py
@@ -3,4 +3,4 @@ import Algorithmia
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    return "hello {}".format(input)
+    return "hello from 'other_hello': https://github.com/zeryx/other_hello/edit/master/src/other_hello.py"
